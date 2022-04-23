2d949695b662d6ed1163d7f7f6cae66012acae6d
junxian0913 <94032164+junxian0913@users.noreply.github.com>
2022-04-23 19:46:16 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -10,19 +10,19 @@ public class Question2
      * Hint: in.nextDouble() to read double
      */
     
-    System.out.println("Enter height: ");
+    
     
      
     Scanner in = new Scanner(System.in);
     double height = in.nextDouble();
    
 
-    System.out.println("Enter weight: ");
+    
     double weight = in.nextDouble(); 
-    System.out.println("weight is: " + weight ); 
+    
 
     double bmi = weight/(height*height);
-    System.out.println("bmi is: " + bmi ); 
+    System.out.println(bmi ); 
     
     
   }
