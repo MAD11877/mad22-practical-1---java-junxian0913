b2dfcd43cfa55edce021c744d74ffabe4acd72d4
junxian0913 <94032164+junxian0913@users.noreply.github.com>
2022-04-23 19:43:30 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
+import java.util.*;
+import java.util.Collections;
+public class Main
 
-public class Question5
 {
   public static void main(String[] args)
   {
@@ -27,6 +29,82 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    
+    
+    
+    int number = in.nextInt();
+    
+
+    List<Integer> l1 = new ArrayList<Integer>();
+    List<Integer> l2 = new ArrayList<Integer>();
+    List<Integer> uniquenum = new ArrayList<Integer>();
+    for (int i=0; i<number; i++ )
+      {
+             
+    
+            int num = in.nextInt();
+            
+            l1.add(i, num);
+            
+            
+      }
+    
+    
+
+    int count = 0;
+    
+    for (int x = 0; x <number; x++){
+      count = 0 ;
+
+      
+      if (x==0){
+        uniquenum.add(l1.get(x)); 
+      }
+      else {
+        if (uniquenum.contains(l1.get(x))){
+          continue; 
+        }
+        else {
+          uniquenum.add(l1.get(x));
+        }
+        
+      }
+      
+      
+      for (int y=0;y<number;y++){
+        
+        if (l1.get(x)== l1.get(y)){
+          count++;}    
+      }
+      l2.add(count);
+        
+    }
+
+    
+    int size = l2.size();
+    
+    
+    Integer max = Collections.max(l2);
+
+     
+    
+    for (int h=0;h<size; h++){
+      if (max == l2.get(h)){
+        
+        System.out.println(uniquenum.get(h));
+        
+      }
+      
+    }
+      
+
+    
+
+    
+    
+    
+    
+         
     
   }
 }
