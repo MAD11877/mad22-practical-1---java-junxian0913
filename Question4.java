edb9ca510adcff4f9bd4f51d32179f71b260bbb0
junxian0913 <94032164+junxian0913@users.noreply.github.com>
2022-04-23 19:45:31 +0800

    Update Question4.java

--- Question4.java
+++ Question4.java
@@ -1,110 +1,37 @@
 import java.util.Scanner;
-import java.util.*;
-import java.util.Collections;
-public class Main
-
+public class Question4
 {
   public static void main(String[] args)
   {
     /**
-     * Prompt the user for number. This input indicates the number of integers the user will be entering next. 
-     * Print out the mode (highest occurrence) from the set of integers. 
+     * Prints a right angle triangle with *. The base of the triangle depends on the integer given.
      *    e.g.
      *     > 5
-     *     > 2
-     *     > 4
-     *     > 1
-     *     > 3
-     *     > 4
-     *     4
+     *     *****
+     *     ****
+     *     ***
+     *     **
+     *     *
      * 
-     *    e.g.
-     *     > 4
-     *     > 2
-     *     > 2
-     *     > 3
-     *     > 3
-     *     2
-     * Hint: Use a loop to get input. Use another 2 loops to find the mode
+     * Hint: 2 loops are required. System.out.print(...) to print on single line.
      */
-     
-    Scanner in = new Scanner(System.in);
+
     
-    
+    Scanner in = new Scanner(System.in); 
     
     int number = in.nextInt();
-    
 
-    List<Integer> l1 = new ArrayList<Integer>();
-    List<Integer> l2 = new ArrayList<Integer>();
-    List<Integer> uniquenum = new ArrayList<Integer>();
-    for (int i=0; i<number; i++ )
+    for (int i = number; 0<i; i--)
+    {
+      for (int x = 0; x< i ; x++ )
       {
-             
-    
-            int num = in.nextInt();
-            
-            l1.add(i, num);
-            
-            
-      }
-    
-    
+          System.out.print("*");
 
-    int count = 0;
-    
-    for (int x = 0; x <number; x++){
-      count = 0 ;
-
-      
-      if (x==0){
-        uniquenum.add(l1.get(x)); 
       }
-      else {
-        if (uniquenum.contains(l1.get(x))){
-          continue; 
-        }
-        else {
-          uniquenum.add(l1.get(x));
-        }
-        
-      }
-      
-      
-      for (int y=0;y<number;y++){
-        
-        if (l1.get(x)== l1.get(y)){
-          count++;}    
-      }
-      l2.add(count);
-        
+      System.out.println();
     }
 
-    
-    int size = l2.size();
-    
-    
-    Integer max = Collections.max(l2);
 
-     
-    
-    for (int h=0;h<size; h++){
-      if (max == l2.get(h)){
-        
-        System.out.println(uniquenum.get(h));
-        
-      }
-      
-    }
-      
 
-    
-
-    
-    
-    
-    
-         
-    
   }
 }
